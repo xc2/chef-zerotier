74ac7503288a69db67232991bf705f17919b07b3
Russell Seymour <russell.seymour@turtlesystems.co.uk>
2017-07-17 11:27:45 +0100

    Moved resource to library so it is using the updated custom resource pattern.
    Signed-off-by: Russell Seymour <russell.seymour@turtlesystems.co.uk>

--- resources/network.rb
+++
@@ -1,7 +0,0 @@
-actions :join, :leave
-default_action :join
-
-attribute :network_id, :kind_of => String, :name_attribute => true, :required => true
-attribute :node_name, :kind_of => String, :required => false
-attribute :auth_token, :kind_of => String, :required => false
-attribute :central_url, :kind_of => String, :default => "https://my.zerotier.com"
