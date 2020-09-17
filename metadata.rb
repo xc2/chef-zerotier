ef19581e6dd3ce2b6bdf5929237f512801ecdd4f
Tim Smith <tsmith84@gmail.com>
2020-09-17 14:04:02 -0700

    Remove deprecated long_description metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,6 @@ maintainer 'Grant Limberg'
 maintainer_email 'grant.limberg@zerotier.com'
 license 'GPL-3.0'
 description 'Installs/Configures ZeroTier'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '1.0.7'
 issues_url 'https://github.com/zerotier/chef-zerotier/issues' if respond_to?(:issues_url)
 source_url 'https://github.com/zerotier/chef-zerotier' if respond_to?(:source_url)
