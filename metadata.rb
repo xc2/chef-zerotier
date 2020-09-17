9d227eceab08b8b47210e65ea8aac3a3cd96b6b2
Tim Smith <tsmith84@gmail.com>
2020-09-17 14:04:18 -0700

    Remove Chef 11 era gating on metadata fields
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -4,8 +4,8 @@ maintainer_email 'grant.limberg@zerotier.com'
 license 'GPL-3.0'
 description 'Installs/Configures ZeroTier'
 version '1.0.7'
-issues_url 'https://github.com/zerotier/chef-zerotier/issues' if respond_to?(:issues_url)
-source_url 'https://github.com/zerotier/chef-zerotier' if respond_to?(:source_url)
+issues_url 'https://github.com/zerotier/chef-zerotier/issues'
+source_url 'https://github.com/zerotier/chef-zerotier'
 
 %w(redhat centos amazon ubuntu debian).each do |os|
     supports os
