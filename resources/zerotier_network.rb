66d492517b75ea09ab2f2885c02e5b31223e7bd1
Kaffi Y. <gh@109cafe.com>
2024-11-27 19:02:51 +0800

    fix: resource (#1)
    * fix: resource name
    
    * fix: resource validation
    
    * fix: refactor

--- resources/zerotier_network.rb
+++ resources/zerotier_network.rb
@@ -1,58 +1,44 @@
-require 'uri'
-
+provides :zerotier_network
 property :network_id, String, name_property: true
-property :node_name, String, required: true
-property :auth_token, String, required: true
+property :node_name, String, required: false
+property :auth_token, String, required: false
+# not used
 property :central_url, String, default: 'https://my.zerotier.com'
 
 action :join do
-  if ::File.exist?(format('/var/lib/zerotier-one/networks.d/%s.conf', network_id))
-    Chef::Log.info(format('Network %s already joined. Skipping.', network_id))
-  else
-    converge_by(format('Joining network %s', network_id)) do
-      join = shell_out(format('/usr/sbin/zerotier-cli join %s', network_id))
-      raise format('Error joining network %s', network_id) if join.error?
+  network_id = new_resource.network_id
+  node_name = (new_resource.node_name or node['fqdn'])
+  auth_token = new_resource.auth_token
+  command = [node['zerotier']['binary']]
+  conf_file = "#{node['zerotier']['data-dir']}/networks.d/#{network_id}.conf"
 
-      if auth_token
-        url = URI.parse(format('%s/api/network/%s/member/%s/', central_url, network_id, node['zerotier']['node_id']))
+  unless auth_token.to_s.empty?
+    command << "-T#{auth_token}"
+  end
 
-        netinfo = {
-            networkId: network_id,
-            nodeId: node['zerotier']['node_id'],
-            name: node_name,
-            config: {
-                nwid: network_id,
-                authorized: true,
-            },
-        }
+  command << "join" << network_id
 
-        response = Net::HTTP.start(url.host, url.port, use_ssl: url.scheme == 'https') do |http|
-          post = Net::HTTP::Post.new(url, 'Content-Type' => 'application/json')
-          post.add_field('Authorization', format('Bearer %s', auth_token))
-          post.body = netinfo.to_json
-          http.request(post)
-        end
-
-        case response
-        when Net::HTTPSuccess
-        # do nothing
-        else
-          shell_out(format('/usr/sbin/zerotier-cli leave %s', network_id))
-          error = JSON.parse(response.body)
-          raise format('Error %s authorizing network: %s: %s', response.code, error['type'], error['message'])
-        end
-      end
-    end
+  execute "join #{network_id}" do
+    command command
+    not_if { ::File.exist?(conf_file) }
   end
 end
 
 action :leave do
-  if ::File.exist?(format('/var/lib/zerotier-one/networks.d/%s.conf', network_id))
-    converge_by(format('Leaving network %s', network_id)) do
-      leave = shell_out(format('/usr/sbin/zerotier-cli leave %s', network_id))
-      raise format('Error leaving network %s', network_id) if leave.error?
-    end
-  else
-    Chef::Log.warn(format('Network %s is not joined. Skipping', network_id))
+  network_id = new_resource.network_id
+  node_name = (new_resource.node_name or node['fqdn'])
+  auth_token = new_resource.auth_token
+  command = [node['zerotier']['binary']]
+  conf_file = "#{node['zerotier']['data-dir']}/networks.d/#{network_id}.conf"
+
+  unless auth_token.to_s.empty?
+    command << "-T#{auth_token}"
+  end
+
+  command << "leave" << network_id
+
+  execute "leave #{network_id}" do
+    command command
+    only_if { ::File.exist?(conf_file) }
   end
 end
