1f4112268af726e951fae966dedebbec1db946d8
xc2 <gh@109cafe.com>
2024-11-30 14:53:06 +0800

    fix: ohai and network existing check

--- ohai/zerotier_info.rb
+++ ohai/zerotier_info.rb
@@ -1,16 +1,39 @@
 Ohai.plugin(:ZerotierInfo) do
   provides 'zerotier_info'
 
-  collect_data(:linux) do
-    zerotier_info Mash.new
-    zerotier_info[:installed] = false
-    if ChefZerotier::Helpers.binary_exists?(node)
-      zerotier_info Mash.from_hash(ChefZerotier::Helpers.command(node, 'info'))
-      zerotier_info[:installed] = true
-      zerotier_info[:node_id] = zerotier_info[:address]
-      zerotier_info.delete(:clock)
-    else
-      Ohai::Log.warn('Cannot find zerotier binary')
+  def cli
+    @cli ||= which('zerotier-cli')
+    @cli
+  end
+  def command(subcommand)
+    unless cli
+      Ohai::Log.debug("zerotier-cli not found in PATH")
+      return nil
     end
+    so = shell_out("#{cli} #{subcommand} -j")
+    if so.exitstatus == 0
+      parse_json(so.stdout)
+    else
+      Ohai::Log.debug(so.stderr)
+      nil
+    end
+  end
+
+  def collect_info
+    info = command('info')
+    if info
+      info = Mash.from_hash(info)
+      info[:installed] = true
+      info[:cli] = cli
+      info[:node_id] = info[:address]
+      info.delete(:clock)
+    else
+      info = Mash.from_hash({installed: false, cli: cli})
+    end
+    info
+  end
+
+  collect_data(:linux) do
+    zerotier_info collect_info
   end
 end
--- ohai/zerotier_networks.rb
+++ ohai/zerotier_networks.rb
@@ -1,17 +1,32 @@
 Ohai.plugin(:ZerotierNetworks) do
   provides 'zerotier_networks'
 
+  def cli
+    @cli ||= which('zerotier-cli')
+    @cli
+  end
+  def command(subcommand)
+    unless cli
+      Ohai::Log.debug("zerotier-cli not found in PATH")
+      return nil
+    end
+    so = shell_out("#{cli} #{subcommand} -j")
+    if so.exitstatus == 0
+      parse_json(so.stdout)
+    else
+      Ohai::Log.debug(so.stderr)
+      nil
+    end
+  end
+
   collect_data(:linux) do
     zerotier_networks Mash.new
-    if ChefZerotier::Helpers.binary_exists?(node)
-      so = ChefZerotier::Helpers.command(node, 'listnetworks')
+    nws = command('listnetworks')
 
-      if so.is_a?(Array)
-        so.each do |network|
-          zerotier_networks[network[:id].to_sym] = Mash.from_hash(network)
-        end
-      else
-        Ohai::Log.warn('Network response should be an array.')
+    if nws.is_a?(Array)
+      nws.each do |nw|
+        nw = Mash.from_hash(nw)
+        zerotier_networks[nw[:id].to_sym] = nw
       end
     end
   end
